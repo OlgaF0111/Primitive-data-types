77273aa8d3a1c6c5de3337808812d7a3fb35ffe8
Olga F <krasota1_00@mail.ru>
2023-02-07 20:23:08 +0700

    README.md

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 50_000; //стоимость билета
+        int b = 20; //стоимость 1 бонусной мили
+        int c = a / b; //количество бонусных милей
+        System.out.println("Количество бонусных милей" + c);
+    }
+}
