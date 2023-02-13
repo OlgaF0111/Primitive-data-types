41835748c8ba247bfae925fa42c575c9ab7068d0
Olga F <krasota1_00@mail.ru>
2023-02-13 20:18:38 +0700

    changing values

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int a = 50_000; //стоимость билета
-        int b = 20; //стоимость 1 бонусной мили
-        int c = a / b; //количество бонусных милей
-        System.out.println("Количество бонусных милей" + c);
+        int cost = 50_000; //стоимость билета
+        int bonus = 20; //стоимость 1 бонусной мили
+        int miles = cost / bonus; //количество бонусных милей
+        System.out.println("Количество бонусных милей" + miles);
     }
 }
